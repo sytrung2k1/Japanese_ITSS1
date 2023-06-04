c93943960f87981fbb4f2b187130b7226d662b9e
Arima057nn <tjendung97pain@gmail.com>
2023-06-04 16:30:28 +0700

    task header

--- Frontend/src/components/common/Header/index.js
+++ Frontend/src/components/common/Header/index.js
@@ -1,9 +1,26 @@
 import classNames from "classnames/bind";
 import styles from "./Header.module.scss";
-
+import { Avatar } from "@mui/material";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 const cx = classNames.bind(styles);
 
 function Header() {
-  return <h1 className={cx("wrapper")}>Header</h1>;
+  return (
+    <div className={cx("wrapper")}>
+      <div className={cx("container")}>
+        <div className={cx("logo")}>Vlean</div>
+        <div className={cx("option")}>
+          <NotificationsNoneIcon sx={{ fontSize: 40 }} />
+          <Avatar
+            alt="Remy Sharp"
+            src="https://d1hjkbq40fs2x4.cloudfront.net/2017-08-21/files/landscape-photography_1645.jpg"
+            sx={{ width: 48, height: 48, margin: "0 0 0 12px" }}
+          />
+          <KeyboardArrowDownIcon fontSize="large" />
+        </div>
+      </div>
+    </div>
+  );
 }
 export default Header;
